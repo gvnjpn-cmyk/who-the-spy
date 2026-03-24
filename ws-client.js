85baa49ebc800aebd77da333322118eeb1b98c53
gvnjpn-cmyk <gvnjpn@gmail.com>
2026-03-24 09:50:49 +0700

    Update ws-client.js

--- ws-client.js
+++ ws-client.js
@@ -1,6 +1,6 @@
 // ws-client.js — WebSocket client untuk Noctyra Games
 // Ganti URL sesuai server Pterodactyl kamu
-const WS_URL = 'wss://erineoshi.jkt48-private.com:3037';
+const WS_URL = 'wss://panelsaluler.jhonaley.net:2066';
 
 class NoctyraWS {
   constructor() {
